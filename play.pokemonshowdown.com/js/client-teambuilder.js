0e6fc6e67503326326fe1c864784ef8273567be2
SanjiTheLord <98240172+SanjiTheLord@users.noreply.github.com>
2026-01-23 19:12:44 -0500

    Add files via upload

--- play.pokemonshowdown.com/js/client-teambuilder.js
+++ play.pokemonshowdown.com/js/client-teambuilder.js
@@ -1240,7 +1240,6 @@
 				buf += '</form></div>';
 			}
 			this.$el.html('<div class="teamwrapper">' + buf + '</div>');
-			this._tryGen5AniSprites();
 			this.$(".teamedit textarea").focus().select();
 			if ($(window).width() < 640) this.show();
 		},
@@ -1255,7 +1254,7 @@
 					buf += '<div class="setmenu setmenu-left"><button name="undeleteSet" class="button"><i class="fa fa-undo"></i> Undo Delete</button></div>';
 				}
 				buf += '<div class="setmenu"><button name="importSet"><i class="fa fa-upload"></i>Import</button></div>';
-				buf += '<div class="setchart" data-spriteid="0" data-spritedir="sprites/gen5" data-shiny="0" style="background-image:url(' + Dex.resourcePrefix + 'sprites/gen5/0.png);"><div class="setcol setcol-icon"><div class="setcell-sprite"></div><div class="setcell setcell-pokemon"><label>Pok&eacute;mon</label><input type="text" name="pokemon" class="textbox chartinput" value="" autocomplete="off" /></div></div></div>';
+				buf += '<div class="setchart" style="background-image:url(' + Dex.resourcePrefix + 'sprites/gen5/0.png);"><div class="setcol setcol-icon"><div class="setcell-sprite"></div><div class="setcell setcell-pokemon"><label>Pok&eacute;mon</label><input type="text" name="pokemon" class="textbox chartinput" value="" autocomplete="off" /></div></div></div>';
 				buf += '</li>';
 				return buf;
 			}
@@ -1263,9 +1262,7 @@
 			buf += '<div class="setchart-nickname">';
 			buf += '<label>Nickname</label><input type="text" name="nickname" class="textbox" value="' + BattleLog.escapeHTML(set.name || '') + '" placeholder="' + BattleLog.escapeHTML(species.baseSpecies) + '" />';
 			buf += '</div>';
-			var __tb_data = Dex.getTeambuilderSpriteData(set, this.curTeam.gen);
-			var __tb_style = Dex.getTeambuilderSprite(set, this.curTeam.gen);
-			buf += '<div class="setchart" data-spriteid="' + __tb_data.spriteid + '" data-spritedir="' + __tb_data.spriteDir + '" data-shiny="' + (__tb_data.shiny ? 1 : 0) + '" style="' + __tb_style + ';">';
+			buf += '<div class="setchart" style="' + Dex.getTeambuilderSprite(set, this.curTeam.gen) + ';">';
 
 			// icon
 			buf += '<div class="setcol setcol-icon">';
@@ -1881,7 +1878,6 @@
 			buf += '</div>';
 
 			this.$el.html('<div class="teamwrapper">' + buf + '</div>');
-			this._tryGen5AniSprites();
 			if ($(window).width() < 640) this.show();
 			this.$chart = this.$('.teambuilder-results');
 			this.search = new BattleSearch(this.$chart, this.$chart);
@@ -1897,7 +1893,6 @@
 		updateSetTop: function () {
 			this.$('.teambar').html(this.renderTeambar());
 			this.$('.teamchart').first().html(this.renderSet(this.curSet, this.curSetLoc));
-			this._tryGen5AniSprites(this.$('.teamchart').first().find('.setchart'));
 		},
 		renderTeambar: function () {
 			var buf = '';
@@ -1936,12 +1931,7 @@
 			var set = this.curSet;
 			if (!set) return;
 
-			var __tb_data_upd = Dex.getTeambuilderSpriteData(set, this.curTeam.gen);
 			this.$('.setchart').attr('style', Dex.getTeambuilderSprite(set, this.curTeam.gen));
-			this.$('.setchart').attr('data-spriteid', __tb_data_upd.spriteid);
-			this.$('.setchart').attr('data-spritedir', __tb_data_upd.spriteDir);
-			this.$('.setchart').attr('data-shiny', __tb_data_upd.shiny ? 1 : 0);
-			this._tryGen5AniSprites(this.$('.setchart'));
 
 			this.$('.pokemonicon-' + this.curSetLoc).css('background', Dex.getPokemonIcon(set).substr(11));
 
@@ -1953,41 +1943,6 @@
 			}
 
 			this.updateStatGraph();
-		},
-
-		_tryGen5AniSprites: function($els) {
-			var $targets;
-			if ($els) {
-				$targets = ($els instanceof jQuery) ? $els : this.$($els);
-			} else {
-				$targets = this.$('.setchart');
-			}
-			var self = this;
-			$targets.each(function() {
-				var $el = $(this);
-				if ($el.data('gen5AniTried')) return;
-				$el.data('gen5AniTried', 1);
-				var spritedir = $el.attr('data-spritedir') || '';
-				if (spritedir.indexOf('gen5') === -1) return;
-				var spriteid = $el.attr('data-spriteid');
-				if (!spriteid) return;
-				var shiny = $el.attr('data-shiny') === '1';
-				var shinySuffix = shiny ? '-shiny' : '';
-				var gifUrl = Dex.resourcePrefix + 'sprites/gen5ani' + shinySuffix + '/' + spriteid + '.gif';
-				var img = new Image();
-				img.onload = function() {
-					var currentStyle = $el.attr('style') || '';
-					var newStyle;
-					if (/url\(/.test(currentStyle)) {
-						newStyle = currentStyle.replace(/url\([^\)]+\)/, 'url(' + gifUrl + ')');
-					} else {
-						newStyle = 'background-image:url(' + gifUrl + ');' + currentStyle;
-					}
-					$el.attr('style', newStyle);
-				};
-				img.onerror = function() {};
-				img.src = gifUrl;
-			});
 		},
 		updateStatGraph: function () {
 			var set = this.curSet;
@@ -3597,7 +3552,7 @@
 			var gen = Math.max(this.room.curTeam.gen, species.gen);
 			var dir = gen > 5 ? 'dex' : 'gen' + gen;
 			if (Dex.prefs('nopastgens')) gen = 'dex';
-			if (Dex.prefs('bwgfx') && dir === 'dex') gen = 'gen5';
+			if (Dex.prefs('bwgfx') && dir === 'dex') gen = 'gen5ani';
 			spriteDir += dir;
 			if (dir === 'dex') {
 				spriteSize = 120;
